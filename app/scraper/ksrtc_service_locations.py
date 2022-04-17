10a48b322a5e48278169c184a214e6dc6b07ec2b
thameemk612 <thameemk612@gmail.com>
2022-04-17 23:31:56 +0530

    add: scraper add: flake8,flake8-annotations

---
+++ app/scraper/ksrtc_service_locations.py
@@ -0,0 +1,10 @@
+#  Project : KSRTC Seat Availability Notification System
+#  Filename : ksrtc_service_locations.py
+#  Author : blacklist
+#  Current modification time : Sun, 17 Apr 2022 at 11:14 PM India Standard Time
+#  Last modified time : Sun, 17 Apr 2022 at 11:14 PM India Standard Time
+
+class KSRTCServiceLocations:
+
+    def get_ksrtc_service_locations(self) -> None:
+        pass
