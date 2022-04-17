5dd3ae6d2620bfd108103c4382ea807cc7fc6cc8
thameemk612 <thameemk612@gmail.com>
2022-04-18 00:05:30 +0530

    change: KSRTCServiceLocations

--- app/scraper/ksrtc_service_locations.py
+++ app/scraper/ksrtc_service_locations.py
@@ -4,7 +4,17 @@
 #  Current modification time : Sun, 17 Apr 2022 at 11:14 PM India Standard Time
 #  Last modified time : Sun, 17 Apr 2022 at 11:14 PM India Standard Time
 
+import requests
+
+from settings import KSRTC_HOME_URL
+
+
 class KSRTCServiceLocations:
+
+    @staticmethod
+    def get_html_response():
+        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+        r = requests.get(KSRTC_HOME_URL, headers=headers)
 
     def get_ksrtc_service_locations(self) -> None:
         pass
