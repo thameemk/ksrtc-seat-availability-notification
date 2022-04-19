bafaf9cfc9c3eb59bb7b08adeda9b03950ca5a3b
thameemk612 <thameemk612@gmail.com>
2022-04-19 23:48:06 +0530

    change: KSRTCServiceLocations - request

--- app/scraper/ksrtc_service_locations.py
+++ app/scraper/ksrtc_service_locations.py
@@ -6,15 +6,24 @@
 
 import requests
 
+from bs4 import BeautifulSoup
+
 from settings import KSRTC_HOME_URL
 
 
 class KSRTCServiceLocations:
 
-    @staticmethod
-    def get_html_response():
-        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-        r = requests.get(KSRTC_HOME_URL, headers=headers)
+    def __init__(self):
+        r = requests.get(KSRTC_HOME_URL, headers={'User-Agent': 'Mozilla/5.0'})
+        if r.status_code == 200:
+            soup = BeautifulSoup(r.content, 'lxml')
+            a = soup
+        else:
+            raise Exception(f"{r.status_code} - {r.reason}")
 
     def get_ksrtc_service_locations(self) -> None:
         pass
+
+
+if __name__ == '__main__':
+    KSRTCServiceLocations()
