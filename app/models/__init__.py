e42cbb58d8f0c6d36c1953c6c39185ac5adb1329
thameemk612 <thameemk612@gmail.com>
2022-05-16 00:13:10 +0530

    add: notification model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,3 +3,7 @@
 #  Author : blacklist
 #  Current modification time : Sat, 7 May 2022 at 5:17 PM India Standard Time
 #  Last modified time : Sat, 7 May 2022 at 5:17 PM India Standard Time
+
+from .service_location import ServiceLocationModel
+from .user import UserModel
+from .notification import NotificationModel
