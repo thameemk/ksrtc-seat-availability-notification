ee9b8d81357fce98228e08253e960bd1f889ed8a
thameemk612 <thameemk612@gmail.com>
2022-05-15 19:38:01 +0530

    change: service location add: user model

--- app/models/service_location.py
+++ app/models/service_location.py
@@ -3,10 +3,13 @@
 #  Author : blacklist
 #  Current modification time : Sat, 7 May 2022 at 8:08 PM India Standard Time
 #  Last modified time : Sat, 7 May 2022 at 8:08 PM India Standard Time
-from fireo.fields import TextField, IDField
+import pendulum
+from fireo.fields import TextField, IDField, DateTime
 from fireo.models import Model
 
 
-class ServiceLocation(Model):
+class ServiceLocationModel(Model):
     location_id = IDField(required=True)
     location_name = TextField(required=True)
+    created_on = DateTime(required=True, default=pendulum.now().utcnow)
+    modified_on = DateTime(required=True, default=pendulum.now().utcnow)
---
+++ app/models/user.py
@@ -0,0 +1,16 @@
+#  Project : KSRTC Seat Availability Notification System
+#  Filename : user.py
+#  Author : blacklist
+#  Current modification time : Sun, 15 May 2022 at 7:33 PM India Standard Time
+#  Last modified time : Sun, 15 May 2022 at 7:33 PM India Standard Time
+import pendulum
+from fireo.fields import TextField, NumberField, DateTime, BooleanField
+from fireo.models import Model
+
+
+class UserModel(Model):
+    full_name = TextField(required=True)
+    phone = NumberField(required=True)
+    is_active = BooleanField(required=True, default=False)
+    created_on = DateTime(required=True, default=pendulum.now().utcnow)
+    modified_on = DateTime(required=True, default=pendulum.now().utcnow)
