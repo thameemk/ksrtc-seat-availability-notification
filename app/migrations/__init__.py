3a9882f4a01ccaac175ee757ec3a40592abc0b47
thameemk612 <thameemk612@gmail.com>
2022-05-22 23:57:19 +0530

    fix: flake8

--- app/migrations/__init__.py
+++ app/migrations/__init__.py
@@ -3,4 +3,3 @@
 #  Author : thameem
 #  Current modification time : Thu, 19 May 2022 at 8:06 PM India Standard Time
 #  Last modified time : Mon, 16 May 2022 at 10:51 PM India Standard Time
-
--- app/views/home.py
+++ app/views/home.py
@@ -3,9 +3,11 @@
 #  Author : thameem
 #  Current modification time : Thu, 19 May 2022 at 8:06 PM India Standard Time
 #  Last modified time : Thu, 19 May 2022 at 12:23 AM India Standard Time
-
+from beartype import beartype
+from django.core.handlers.wsgi import WSGIRequest
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse(f"Connection Successful")
+@beartype
+def index(request: WSGIRequest) -> 'HttpResponse':
+    return HttpResponse("Connection Successful")
