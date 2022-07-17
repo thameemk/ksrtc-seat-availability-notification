2ed8dbd3ff7c1a2252c1dd1194eaf0879657e995
thameemk612 <thameemk612@gmail.com>
2022-07-18 01:10:22 +0530

    change: auth

--- app/models/user.py
+++ app/models/user.py
@@ -3,10 +3,12 @@
 #  Author : thameem
 #  Current modification time : Thu, 19 May 2022 at 8:06 PM India Standard Time
 #  Last modified time : Wed, 18 May 2022 at 11:50 PM India Standard Time
-
+import logging
 from datetime import datetime
+from typing import Optional
 
-from fireo.fields import TextField, NumberField, DateTime, BooleanField
+from beartype import beartype
+from fireo.fields import TextField, DateTime, BooleanField, IDField, NumberField
 from fireo.models import Model
 
 
@@ -14,8 +16,28 @@ class UserModel(Model):
     class Meta:
         collection_name = "users"
 
+    uid = IDField(required=True)
     full_name = TextField(required=True)
-    phone = NumberField(required=True)
+    phone_number = NumberField(required=True, int_only=True)
     is_deleted = BooleanField(required=True, default=False)
     created_on = DateTime(required=True, default=datetime.now())
     modified_on = DateTime(required=True, default=datetime.now())
+
+    @classmethod
+    @beartype
+    def save_user(cls, firebase_uid: str, phone: int, full_name: str, ) -> 'UserModel':
+        user_obj = UserModel(
+            uid=firebase_uid,
+            phone_number=phone,
+            full_name=full_name
+        ).save()
+        return user_obj
+
+    @classmethod
+    @beartype
+    def get_user(cls, firebase_uid: str) -> (bool, Optional['UserModel']):
+        try:
+            return True, UserModel.collection.get(key=firebase_uid)
+        except Exception as e:
+            logging.error(str(e))
+            return False, None
