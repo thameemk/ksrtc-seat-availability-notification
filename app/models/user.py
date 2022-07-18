5f84d7dac29df56edb57b9472a438947ec7a7eab
thameemk612 <thameemk612@gmail.com>
2022-07-19 01:09:34 +0530

    add: pycharm config change: save and get user, auth call back

--- app/models/user.py
+++ app/models/user.py
@@ -3,12 +3,11 @@
 #  Author : thameem
 #  Current modification time : Thu, 19 May 2022 at 8:06 PM India Standard Time
 #  Last modified time : Wed, 18 May 2022 at 11:50 PM India Standard Time
-import logging
 from datetime import datetime
 from typing import Optional
 
 from beartype import beartype
-from fireo.fields import TextField, DateTime, BooleanField, IDField, NumberField
+from fireo.fields import TextField, DateTime, BooleanField, IDField
 from fireo.models import Model
 
 
@@ -18,26 +17,24 @@ class UserModel(Model):
 
     uid = IDField(required=True)
     full_name = TextField(required=True)
-    phone_number = NumberField(required=True, int_only=True)
+    phone_number = TextField(required=True)
     is_deleted = BooleanField(required=True, default=False)
     created_on = DateTime(required=True, default=datetime.now())
     modified_on = DateTime(required=True, default=datetime.now())
 
     @classmethod
     @beartype
-    def save_user(cls, firebase_uid: str, phone: int, full_name: str, ) -> 'UserModel':
-        user_obj = UserModel(
-            uid=firebase_uid,
-            phone_number=phone,
-            full_name=full_name
-        ).save()
-        return user_obj
+    def save_user(cls, firebase_uid: str, phone: str, full_name: str, ) -> 'UserModel':
+        user = UserModel()
+        user.uid = firebase_uid
+        user.phone_number = phone
+        user.full_name = full_name
+        user.save()
+        return user
 
     @classmethod
     @beartype
-    def get_user(cls, firebase_uid: str) -> (bool, Optional['UserModel']):
-        try:
-            return True, UserModel.collection.get(key=firebase_uid)
-        except Exception as e:
-            logging.error(str(e))
-            return False, None
+    def get_user(cls, firebase_uid: str) -> Optional['UserModel']:
+        _user = UserModel()
+        _user.uid = firebase_uid
+        return cls.collection.get(_user.key)
