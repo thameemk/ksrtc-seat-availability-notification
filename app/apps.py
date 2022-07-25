7e7cf9d2245489222905df8f500ae6847da73105
thameemk612 <thameemk612@gmail.com>
2022-07-25 23:48:35 +0530

    change: resource root, resource root

--- app/apps.py
+++ app/apps.py
@@ -3,10 +3,3 @@
 #  Author : thameem
 #  Current modification time : Thu, 19 May 2022 at 8:06 PM India Standard Time
 #  Last modified time : Mon, 16 May 2022 at 11:06 PM India Standard Time
-
-from django.apps import AppConfig
-
-
-class AppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app'
