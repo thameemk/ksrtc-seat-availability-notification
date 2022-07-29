9cbb8022c6bf2b8d2095eb25a82c627e94bf0e2b
thameemk612 <thameemk612@gmail.com>
2022-07-29 09:57:07 +0530

    change: login.html

--- main/settings.py
+++ main/settings.py
@@ -124,4 +124,4 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-KSRTC_HOME_URL = config('KSRTC_HOME_URL')
+KSRTC_HOME_URL = config('KSRTC_HOME_URL', default="")
