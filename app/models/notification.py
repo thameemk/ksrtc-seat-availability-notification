b92b5fd1cf92be7377472844f7619530e3bf224b
thameemk612 <thameemk612@gmail.com>
2022-08-04 22:43:01 +0530

    change: notification

--- app/models/notification.py
+++ app/models/notification.py
@@ -3,8 +3,10 @@
 #  Author : thameem
 #  Current modification time : Thu, 19 May 2022 at 8:06 PM India Standard Time
 #  Last modified time : Wed, 18 May 2022 at 11:36 PM India Standard Time
+from typing import Optional
 
 import pendulum
+from beartype import beartype
 from fireo.fields import ReferenceField, BooleanField, DateTime, NumberField
 from fireo.models import Model
 
@@ -25,3 +27,8 @@ class NotificationModel(Model):
     receive_notification_up_to = DateTime(required=True)
     created_on = DateTime(required=True, default=pendulum.now().utcnow)
     modified_on = DateTime(required=True, default=pendulum.now().utcnow)
+
+    @classmethod
+    @beartype
+    def get_notifications(cls, user: str) -> Optional[list['NotificationModel']]:
+        return [each for each in cls.collection.fetch() if each.user == user]
