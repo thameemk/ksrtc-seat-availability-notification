f3549b5e8c741792b3e06c63f72aa8b0b8504b9d
thameemk612 <thameemk612@gmail.com>
2022-08-30 21:34:16 +0530

    change: notifications

--- app/views/user.py
+++ app/views/user.py
@@ -3,7 +3,9 @@
 #  Author : thameem
 #  Current modification time : Mon, 23 May 2022 at 12:05 AM India Standard Time
 #  Last modified time : Mon, 23 May 2022 at 12:05 AM India Standard Time
-from datetime import datetime
+import threading
+import uuid
+from datetime import datetime, date
 
 from beartype import beartype
 from django.contrib import messages
@@ -43,25 +45,27 @@ class User:
 
         date_of_departure = datetime.strptime(request.POST['date_of_departure'], "%Y-%m-%d")
 
-        available_seats = GetServices.get_total_seats(request.POST['leaving_from'], request.POST['going_to'],
-                                                      date_of_departure.date())
-
         _notification_obj = NotificationModel(
+            notification_id=uuid.uuid4().hex,
             leaving_from=leaving_from,
             going_to=going_to,
             date_of_departure=date_of_departure,
             date_of_return=datetime.strptime(request.POST['date_of_return'], "%Y-%m-%d") if request.POST[
                 'date_of_return'] else None,
             user=user,
-            available_seats=available_seats,
+            available_seats=0,
             time_interval=int(request.POST['time_interval']),
             receive_notification_up_to=datetime.strptime(request.POST['receive_notification_up_to'], "%Y-%m-%d"),
         )
 
         notification = NotificationModel.save_notification(_notification_obj)
 
+        threading.Thread(target=User._update_available_seats,
+                         args=(request.POST['leaving_from'], request.POST['going_to'],
+                               date_of_departure.date()))
+
         if notification:
-            messages.success(request, 'The notification was successfully added.')
+            messages.success(request, 'Request in progress, please refresh the page to know the available seats')
         else:
             messages.error(request, 'Some sort of error has occurred.')
 
@@ -69,10 +73,20 @@ class User:
 
     @staticmethod
     @beartype
+    def _update_available_seats(notification: NotificationModel, leaving_from: str, going_to: str,
+                                journey_date: date) -> None:
+
+        available_seats = GetServices.get_total_seats(leaving_from, going_to, journey_date)
+        notification.available_seats = available_seats
+        notification.update()
+
+    @staticmethod
+    @beartype
     def disable_notification(request: WSGIRequest, notification_id: str) -> 'HttpResponse':
         notification_obj = NotificationModel.get_notification(request.session['user'], notification_id)
-        response = True
-        if response is True:
+        notification_obj.is_active = False
+        notification_obj.update()
+        if notification_obj.is_active is False:
             messages.success(request, f'notification with id {notification_id} successfully disabled')
         else:
             messages.error(request, 'Some sort of error has occurred.')
@@ -82,9 +96,13 @@ class User:
     @staticmethod
     @beartype
     def update_seats(request: WSGIRequest, notification_id: str) -> 'HttpResponse':
-        response = True
-        if response is True:
-            messages.success(request, f'notification with id {notification_id} updated successfully')
+        notification_obj = NotificationModel.get_notification(request.session['user'], notification_id)
+
+        if notification_obj:
+            threading.Thread(target=User._update_available_seats,
+                             args=(notification_obj.leaving_from.location_id, notification_obj.going_to.location_id,
+                                   notification_obj.date_of_departure.date()))
+            messages.success(request, 'Request in progress, please refresh the page to know the available seats')
         else:
             messages.error(request, 'Some sort of error has occurred.')
 
