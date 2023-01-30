1bd322d99f30ebf63b4f86947b95297b8b2c56f0
thameemk612 <thameemk612@gmail.com>
2023-01-31 00:20:42 +0530

    add sample test function

---
+++ tests/test_sample.py
@@ -0,0 +1,8 @@
+#  Project : KSRTC Seat Availability Notification System
+#  Filename : test_sample.py
+#  Author : thameem
+#  Last modified time : Tue, 31 Jan 2023 at 12:18 am India Standard Time
+
+
+def test_hello():
+    pass
